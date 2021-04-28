0154b06b27d2e351847f974df1a5eb0bec36bb00
Yann Hamon <yann.hamon@contentful.com>
2021-04-28 22:45:46 +0200

    Update acceptance tests

--- fixtures/acceptance-tests.sh
+++ fixtures/acceptance-tests.sh
@@ -6,16 +6,12 @@ apk add redis bats
 echo "-> Waiting for Redis to start..."
 timeout 30 sh -c 'until redis-cli -h redis -p 6379 PING >/dev/null; do sleep 1; done'
 
-echo "-> Run Bats tests"
-
 echo "-> Filling Redis with Mock Data..."
 chmod a+x /generator
 
 redis-cli -h redis FLUSHDB
-/generator -n 1000 | redis-cli -h redis --pipe
+/generator -n 10000 | redis-cli -h redis --pipe
 DBSIZE=`redis-cli -h redis dbsize`
-echo "Redis has $DBSIZE entries"
-
 
 echo "-> Dumping DB..."
 /redis-dump-go -host redis -output resp >backup
@@ -26,6 +22,10 @@ redis-cli -h redis --pipe <backup
 NEWDBSIZE=`redis-cli -h redis dbsize`
 echo "Redis has $DBSIZE entries"
 
-if [ $DBSIZE -eq $NEWDBSIZE ]; then
-  false
+echo "-> Comparing DB sizes..."
+if [ $DBSIZE -ne $NEWDBSIZE ]; then
+  echo "ERROR - restored DB has $NEWDBSIZE elements, expected $DBSIZE"
+  return 1
+else
+  echo "OK - $NEWDBSIZE elements"
 fi
