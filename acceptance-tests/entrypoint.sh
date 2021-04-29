f5a02bd3179525165765265dfddad7a0e35cde16
Yann Hamon <yann.hamon@contentful.com>
2021-04-29 12:53:33 +0200

    Use bats for acceptance tests

--- acceptance-tests/entrypoint.sh
+++ acceptance-tests/entrypoint.sh
@@ -6,6 +6,4 @@ apk add redis bats ncurses
 echo "-> Waiting for Redis to start..."
 timeout 30 sh -c 'until redis-cli -h redis -p 6379 PING >/dev/null; do sleep 1; done'
 
-chmod a+x /generator
-
 bats --tap -p acceptance.bats
