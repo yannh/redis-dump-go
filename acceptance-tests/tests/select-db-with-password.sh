716c2b70be139cdaa43b75fb2b8bc5d5b5413891
Yann Hamon <yann@mandragor.org>
2022-02-05 23:47:03 +0100

    Fix tls/password usage when selecting a db, add new acceptance test

---
+++ acceptance-tests/tests/select-db-with-password.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+export DB=2
+export REDIS_PORT=6380
+export REDISDUMPGO_AUTH=somepassword
+export REDISCMD="redis-cli -h redis_secure -p $REDIS_PORT --pass $REDISDUMPGO_AUTH -n 2"
+
+echo "-> Filling Redis with Mock Data..."
+$REDISCMD FLUSHDB
+/generator -output resp -type strings -n 100 | $REDISCMD --pipe
+DBSIZE=`$REDISCMD dbsize`
+
+echo "-> Dumping DB..."
+time /redis-dump-go -host redis_secure -n 250 -port $REDIS_PORT -db $DB -output resp >backup
+
+echo "-> Flushing DB and restoring dump..."
+$REDISCMD FLUSHDB
+$REDISCMD --pipe <backup
+NEWDBSIZE=`$REDISCMD dbsize`
+echo "Redis has $DBSIZE entries"
+
+echo "-> Comparing DB sizes..."
+if [ $DBSIZE -ne $NEWDBSIZE ]; then
+  echo "ERROR - restored DB has $NEWDBSIZE elements, expected $DBSIZE"
+  exit 1
+else
+  echo "OK - $NEWDBSIZE elements"
+  exit 0
+fi
