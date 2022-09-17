b2cecc5fd449f74260d0ca26f0c51ea73142d657
Yann Hamon <yann@mandragor.org>
2022-09-17 12:30:01 +0200

    Start multiple redis servers for acceptance tests of auth

--- acceptance-tests/tests/select-db-with-password.sh
+++ acceptance-tests/tests/select-db-with-password.sh
@@ -3,7 +3,7 @@
 export DB=2
 export REDIS_PORT=6380
 export REDISDUMPGO_AUTH=somepassword
-export REDISCMD="redis-cli -h redis_secure -p $REDIS_PORT --pass $REDISDUMPGO_AUTH -n 2"
+export REDISCMD="redis-cli -h redis_with_password -p $REDIS_PORT --pass $REDISDUMPGO_AUTH -n 2"
 
 echo "-> Filling Redis with Mock Data..."
 $REDISCMD FLUSHDB
@@ -11,7 +11,7 @@ $REDISCMD FLUSHDB
 DBSIZE=`$REDISCMD dbsize`
 
 echo "-> Dumping DB..."
-time /redis-dump-go -host redis_secure -n 250 -port $REDIS_PORT -db $DB -output resp >backup
+time /redis-dump-go -host redis_with_password -n 250 -port $REDIS_PORT -db $DB -output resp >backup
 
 echo "-> Flushing DB and restoring dump..."
 $REDISCMD FLUSHDB
--- acceptance-tests/tests/select-db-with-username-password.sh
+++ acceptance-tests/tests/select-db-with-username-password.sh
@@ -4,7 +4,7 @@ export DB=2
 export REDIS_PORT=6380
 export REDIS_USER=test
 export REDISDUMPGO_AUTH=testpassword
-export REDISCMD="redis-cli -h redis_secure -p $REDIS_PORT --user $REDIS_USER --pass $REDISDUMPGO_AUTH -n 2"
+export REDISCMD="redis-cli -h redis_with_username_and_password -p $REDIS_PORT --user $REDIS_USER --pass $REDISDUMPGO_AUTH -n 2"
 echo $REDISCMD
 echo "-> Filling Redis with Mock Data..."
 $REDISCMD FLUSHDB
@@ -12,7 +12,7 @@ $REDISCMD FLUSHDB
 DBSIZE=`$REDISCMD dbsize`
 
 echo "-> Dumping DB..."
-time /redis-dump-go -host redis_secure -n 250 -port $REDIS_PORT -db $DB -user $REDIS_USER -output resp >backup
+time /redis-dump-go -host redis_with_username_and_password -n 250 -port $REDIS_PORT -db $DB -user $REDIS_USER -output resp >backup
 
 echo "-> Flushing DB and restoring dump..."
 $REDISCMD FLUSHDB
